92cd6b739dd01da0f5baafea2f6d4021902fe189
sakibian <sakib192@gmail.com>
2023-09-06 19:07:11 +0600

    expressions
    Signed-off-by: sakibian <sakib192@gmail.com>

--- expressions_and_operators/main.js
+++ expressions_and_operators/main.js
@@ -71,3 +71,86 @@ let baz = true;
 baz += 1;
 
 console.log(baz);
+
+// Assigning to properties
+
+// If an expression evaluates to an object, then the left - hand side of an assignment expression may make assignments to properties of that expression.For example:
+
+const obj = {};
+
+obj.x = 3;
+
+console.log(obj.x);
+console.log(obj);
+
+const key = "y";
+obj[key] = 5;
+console.log(obj[key]);
+console.log(obj);
+
+// If an expression does not evaluate to an object, then assignments to properties of that expression do not assign:
+
+const val = 0;
+val.x = 3;
+console.log(val.x);
+console.log(val);
+
+// Destructuring
+
+const foo1 = ["one", "two", "three"];
+
+const one = foo1[0];
+const two = foo1[1];
+const three = foo1[2];
+console.log(foo1, one, two, three);
+const [four, five, six] = foo1;
+console.log(foo1);
+
+// Evaluation and nesting
+
+function f() {
+  return 0;
+}
+
+function g() {
+  return 1;
+}
+
+let xFunction = f(); // init result 0;
+console.log(xFunction);
+xFunction = g(); // reassigned() result 1
+console.log(xFunction);
+
+let x3;
+const y2 = (x3 = f()); // == const y2 = x3 = f();
+console.log(y2); // Logs the return value of the assignment x = f().
+console.log((x3 = f())); // Logs the return value directly.
+
+// Comparison operators
+
+const var1 = 3;
+const var2 = 4;
+
+let equal = var1 == var2;
+console.log("== :", equal);
+
+let notEqual = var1 != var2;
+console.log("!= :", notEqual);
+
+let strictEqual = var1 === var2;
+console.log("=== :", strictEqual);
+
+let strictNotEqual = var1 !== var2;
+console.log("!== :", strictNotEqual);
+
+let greaterThen = var1 > var2;
+console.log("> :", greaterThen);
+
+let greaterThenOrEqual = var1 >= var2;
+console.log(">= :", greaterThenOrEqual);
+
+let lessThen = var1 < var2;
+console.log("< :", lessThen);
+
+let lessThenOrEqual = var1 <= var2;
+console.log("<= :", lessThenOrEqual);
